f34240439152053815620ba2a9b48ce22afec612
Smith <98213658+crimra@users.noreply.github.com>
2022-04-01 05:42:11 +0100

    other_method

--- Modal/index.js
+++ Modal/index.js
@@ -1,5 +1,7 @@
 var container = document.querySelector('.container')
 var modals = document.querySelectorAll('.modal')
+var open = document.querySelector('.open')
+var close = document.querySelector('.close')
 
 
 modals.forEach( modal => modal.addEventListener('click', Showmodal))
@@ -7,3 +9,11 @@ modals.forEach( modal => modal.addEventListener('click', Showmodal))
 function Showmodal () {
     container.classList.toggle('active')
 }
+
+/*open.addEventListener('click', function(){
+    container.classList.add('active')
+})
+
+close.addEventListener('click', function(){
+    container.classList.remove('active')
+})*/
